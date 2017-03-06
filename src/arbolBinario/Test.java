4a33b0625e8b9334be49d3a3ade8e03f3f68fa80
Pablo <c1pbenedictoc@ieslavereda.es>
2017-03-06 20:07:35 +0100

    Sergio

--- src/arbolBinario/Test.java
+++ src/arbolBinario/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
 		ArbolBinario arbol;
 		Nodo a1, a2, a;
 		PilaVector pila = new PilaVector();
